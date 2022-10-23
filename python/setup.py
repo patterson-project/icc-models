dc18d146a6c40bbe576c61818ace38bc85a0cd3b
canadrian72 <patterson.n.adrian@gmail.com>
2022-10-23 12:37:03 -0400

    Adding auto push to pypi workflow

--- python/setup.py
+++ python/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='python-icc',
-      version='0.0.4',
+      version='0.0.5',
       description='IoT Control Center home automation project packages',
       url='https://github.com/patterson-project/icc-models',
       author='Patterson Project',

9922689a3208c7af8dd80efaa97860cabe7ec8c4
canadrian72 <patterson.n.adrian@gmail.com>
2022-11-20 19:48:07 -0500

    Upgrading version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ Home automation data models, packaged to prevent code duplication throughout mic
 """
 
 setup(name='python-icc',
-      version='0.0.11',
+      version='0.0.12',
       long_description_content_type="text/markdown",
       long_description=README,
       description='IoT Control Center home automation project packages',

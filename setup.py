7f76677007b44b5f980378848cf6f5233a9d9d55
canadrian72 <patterson.n.adrian@gmail.com>
2022-12-10 13:07:30 -0500

    Creating new version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ Home automation data models, packaged to prevent code duplication throughout mic
 
 setup(
     name="python-icc",
-    version="0.0.26",
+    version="0.0.27",
     long_description_content_type="text/markdown",
     long_description=README,
     description="IoT Control Center home automation project packages",
